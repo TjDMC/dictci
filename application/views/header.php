b373623aad3433429d12dcebe07fca604987add1
ahotsuki <steinbergcurse@gmail.com>
2018-06-19 13:22:41 +0800

    n

--- application/views/header.php
+++ application/views/header.php
@@ -16,10 +16,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
     <script src="<?= base_url().'js/app.js'?>"></script>
 
-	<!-- Header -->
-	<div style="background-color:black;border: 1px solid #D0D0D0;margin: 10px;box-shadow: 0 0 8px #D0D0D0;">
-		<br/><br/><br/><br/><br/>Sample header
-	</div>
 </head>
 
 <body>
