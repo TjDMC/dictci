a32bc67c8cae374b0abbbbe5cadadaac5f2551e0
ahotsuki <steinbergcurse@gmail.com>
2018-06-19 10:48:14 +0800

    popo

--- application/views/header.php
+++ application/views/header.php
@@ -6,8 +6,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html>
 <head>
     <title>DICT</title>
-
     <script src="<?= base_url().'js/angular.min.js' ?>"></script>
+	<div style="background-color:black;border: 1px solid #D0D0D0;margin: 10px;box-shadow: 0 0 8px #D0D0D0;">
+		<br/><br/><br/><br/><br/>Sample header
+	</div>
 </head>
 
 <body>
