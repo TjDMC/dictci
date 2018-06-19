a007535aead425783f8e901ddff41191a2155834
ahotsuki <steinbergcurse@gmail.com>
2018-06-19 11:10:32 +0800

    asd

--- application/views/header.php
+++ application/views/header.php
@@ -9,12 +9,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<link href="<?= base_url() ?>/css/bootstrap.min.css">
 	<link href="<?= base_url() ?>/css/example.css" type="text/css" rel="stylesheet">
     <script src="<?= base_url().'js/angular.min.js' ?>"></script>
-	
-	<!-- Header -->
-	<div style="background-color:black;border: 1px solid #D0D0D0;margin: 10px;box-shadow: 0 0 8px #D0D0D0;">
-		<br/><br/><br/><br/><br/>Sample header
-	</div>
 </head>
 
 <body>
-<div id="wrapper">
+<!--<div id="wrapper">-->
+<!-- Header -->
+	<div style="background-color:black;border: 1px solid #D0D0D0;margin: 10px;box-shadow: 0 0 8px #D0D0D0;">
+		<br/><br/><br/><br/><br/>Sample header<br/>
+		<button>Calendar</button>
+		<button>Employee</button>
+	</div>
