87426eeabfe8e0f9fba9e2e8d9e128042beb14fb
Paul Jeremiah Naraval <pdnaraval@up.edu.ph>
2018-06-19 14:48:30 +0800

    better header

--- application/views/header.php
+++ application/views/header.php
@@ -4,7 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <html ng-app="app">
 <head ng-controller="initializer" ng-init="init('<?=base_url()?>','<?= $this->security->get_csrf_token_name()?>','<?= $this->security->get_csrf_hash()?>')">
+
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	
     <title>DICT</title>
 	
 	
@@ -15,11 +17,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <script src="<?= base_url().'js/angular.min.js' ?>"></script>
 
     <script src="<?= base_url().'js/app.js'?>"></script>
-
-	<!-- Header -->
-	<div style="background-color:black;border: 1px solid #D0D0D0;margin: 10px;box-shadow: 0 0 8px #D0D0D0;">
-		<br/><br/><br/><br/><br/>Sample header
-	</div>
 </head>
 
 <body>
