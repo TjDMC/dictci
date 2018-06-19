88e488d175c70e0610136a337381ced03044c142
Paul Jeremiah Naraval <pdnaraval@up.edu.ph>
2018-06-19 10:58:10 +0800

    css

--- application/views/footer.php
+++ application/views/footer.php
@@ -4,5 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<div style="background-color:black;border: 1px solid #D0D0D0;margin: 10px;box-shadow: 0 0 8px #D0D0D0;">
 		<br/><br/><br/><br/><br/>Sample footer
 	</div>
+	
+</div>
 </body>
 </html>
