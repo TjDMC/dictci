45cbc25ab030c45aacb5cfcf8632365e11693e7d
ahotsuki <steinbergcurse@gmail.com>
2018-06-20 13:11:18 +0800

    dsd

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -5,5 +5,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <div id="container">
 	<p>HA! Ha! It Works!</p>
-	<a href="<?= base_url().'calendar'?>"><button>Calendar Samp</button></a>
 </div>
