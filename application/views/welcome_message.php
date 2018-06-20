85ba1d50010a798133fd11e6de3fba8681a142f9
Paul Jeremiah Naraval <pdnaraval@up.edu.ph>
2018-06-20 17:24:30 +0800

    betterStyle

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,5 +4,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 <div id="container">
-	<h2>HA! Ha! It Works!</h2>
+	<div class="row" style="padding-top: 7%">
+		<div class="col-sm-6">
+			<div>
+				<a href="<?= base_url()?>employee" style="text-decoration:none; color:mediumaquamarine;">
+					<div class="thumbnail" style="text-align:center; font-size:50px;">
+						<span class="fas fa-user-tie" style="font-size:100px"></span></br>
+						Employee
+					</div>
+				</a>
+			</div>
+		</div>
+		<div class="col-sm-6">
+			<div>
+				<a href="<?= base_url()?>calendar" style="text-decoration:none; color:red;">
+					<div class="thumbnail" style="text-align:center; font-size:50px;">
+						<span class="far fa-calendar-alt" style="font-size:100px"></span></br>
+						Calendar
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>
 </div>
