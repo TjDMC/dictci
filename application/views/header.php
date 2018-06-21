86744f93ef4d250e59eda9f6a44392f64469d43f
Paul Jeremiah Naraval <pdnaraval@up.edu.ph>
2018-06-21 09:45:39 +0800

    ya

--- application/views/header.php
+++ application/views/header.php
@@ -8,7 +8,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
-    <title>DICT</title>
+    <title>&#124;  DICT</title>
 	<link rel="icon" href="<?= base_url()?>/favicon.png" type="image/gif">
 
 
