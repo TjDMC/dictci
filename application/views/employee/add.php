3736bc1a359e9267d9e621d5d34ea2da6d4ea31e
TjDMC <tjustin.asdf@outlook.com>
2018-06-22 10:20:11 +0800

    bootstrappping pt.3

--- application/views/employee/add.php
+++ application/views/employee/add.php
@@ -6,36 +6,53 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <div>
         <h1>Add Employee</h1>
     </div>
-    <form>
+    <form ng-submit="add()">
+        <div class="form-group autocomplete">
+            <label>Employee No: </label>
+            <input class="form-control" type="text" ng-model="employee.emp_no" pattern="[0-9]{7}" required>
+        </div>
+        <div class="form-group autocomplete">
+            <label>Last Name: </label>
+            <input class="form-control" type="text" ng-model="employee.last_name" required>
+        </div>
+        <div class="form-group autocomplete">
+            <label>First Name: </label>
+            <input class="form-control" type="text" ng-model="employee.first_name" required>
+        </div>
+        <div class="form-group autocomplete">
+            <label>Middle Name: </label>
+            <input class="form-control" type="text" ng-model="employee.middle_name" required>
+        </div>
+        <div class="dropdown form-group" style="max-width:400px">
+            <label>First Day:</label>
+            <a id="firstday" data-toggle="dropdown" data-target="dropdown" href="#">
+                <div class="input-group">
+                    <input data-date-time-input="MMMM DD, YYYY" class="form-control" type="text" data-ng-model="employee.first_day">
+                    <span class="input-group-addon"><i class="far fa-calendar-alt"></i></span>
+                </div>
+            </a>
+            <ul class="dropdown-menu">
+                <datetimepicker  data-ng-model="employee.first_day" data-datetimepicker-config="{ dropdownSelector:'#firstday',minView:'day' }"></datetimepicker>
+            </ul>
+        </div>
+        <div class="form-group autocomplete">
+            <label>Salary: </label>
+            <input class="form-control" type="text" ng-model="employee.salary">
+        </div>
+        <div class="form-group autocomplete">
+            <label>Position: </label>
+            <input class="form-control" type="text" ng-model="employee.position">
+        </div>
+        <div class="form-group autocomplete" ng-init="employee.vac_leave_bal=0">
+            <label>Vacation Leave Balance: </label>
+            <input class="form-control" type="number" ng-model="employee.vac_leave_bal" required>
+        </div>
+        <div class="form-group autocomplete" ng-init="employee.sick_leave_bal=0">
+            <label>Sick Leave Balance: </label>
+            <input class="form-control" type="number" ng-model="employee.sick_leave_bal" required>
+        </div>
         <p>
-            Employee No: <input type="text" ng-model="employee.emp_no"/>
-        </p>
-        <p>
-            Last Name: <input type="text" ng-model="employee.last_name"/>
-        </p>
-        <p>
-            Fist Name: <input type="text" ng-model="employee.first_name"/>
-        </p>
-        <p>
-            Middle Name: <input type="text" ng-model="employee.middle_name"/>
-        </p>
-        <p>
-            First Day: <input type="text" ng-model="employee.first_day"/>
-        </p>
-        <p>
-            Salary: <input type="text" ng-model="employee.salary"/>
-        </p>
-        <p>
-            Position: <input type="text" ng-model="employee.position"/>
-        </p>
-        <p>
-            Vacation Leave Balance: <input type="text" ng-model="employee.vac_leave_bal"/>
-        </p>
-        <p>
-            Sick Leave Balance: <input type="text" ng-model="employee.sick_leave_bal"/>
-        </p>
-        <p>
-            <input type="Submit" ng-click="add()"/>
+            <button class="btn btn-primary" type="Submit">Add</button>
         </p>
     </form>
 </div>
