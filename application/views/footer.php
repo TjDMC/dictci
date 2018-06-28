764487840597a42a581967c5bb208bd275ed5c19
TjDMC <tjustin.asdf@outlook.com>
2018-06-28 10:09:41 +0800

    Using sb-admin template

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,57 +2,5 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 
-
-		</div>
-
-		<footer style="background-color:#1a1a1a">
-			<div style="color:#a6a6a6;float:right" class="p-2" >
-				© 2018. All Rights Reserved
-			</div>
-		</footer>
-	</div>
-
-	<!-- SideBar -->
-	<div id="sidebar-wrapper">
-		<ul class="sidebar-nav">
-			<li class="sidebar-brand">
-				<a href="<?= base_url() ?>">DICT</a>
-			</li>
-			<li style="margin-top: 2px; margin-bottom: 5px">
-				<a href="<?= base_url() ?>">Home</a>
-			</li>
-			<li>
-				<a data-toggle="collapse" data-target="#employee" aria-expanded="false">Employee</a>
-				<div id="employee" class="collapse smaller">
-					<a href="<?= base_url()?>employee/add">&nbsp;&nbsp;&nbsp;&nbsp;Add New</a></br>
-					<a href="<?= base_url()?>employee/leaveapplication">&nbsp;&nbsp;&nbsp;&nbsp;Application for Leave</a></br>
-					<a href="<?= base_url()?>employee">&nbsp;&nbsp;&nbsp;&nbsp;View List</a></br>
-					<a href="#">&nbsp;&nbsp;&nbsp;&nbsp;Resignation</a>
-				</div>
-			</li>
-			<li>
-				<a data-toggle="collapse" data-target="#choice_calendar" aria-expanded="false">Calendar</a>
-				<div id="choice_calendar" class="collapse smaller">
-					<a href="#">&nbsp;&nbsp;&nbsp;&nbsp;Add Holiday</a></br>
-					<a href="<?= base_url().'calendar' ?>">&nbsp;&nbsp;&nbsp;&nbsp;View Calendar</a></br>
-					<a href="#">&nbsp;&nbsp;&nbsp;&nbsp;Suspended Work</a>
-				</div>
-			</li>
-			<li ng-controller="login">
-				<a ng-if="<?=$this->ion_auth->logged_in()?1:0?>==true" ng-click="logout()">Logout</a>
-			</li>
-		</ul>
-	</div>
-	<!-- /#sidebar -->
-
-</div>
-
-<script>
-	$("#menu-toggle").click(function(e) {
-		e.preventDefault();
-		$("#wrapper").toggleClass("active");
-	});
-</script>
-
 </body>
 </html>
--- application/views/header.php
+++ application/views/header.php
@@ -8,7 +8,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
-    <title>&#124;  DICT</title>
+    <title>Department of Information and Communications Technology</title>
 	<link rel="icon" href="<?= base_url()?>/favicon.png" type="image/gif">
 
 
@@ -38,20 +38,166 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </head>
 
 <body>
-<div id="wrapper">
-	<div id="content">
-	<!-- Header -->
-	<header>
-		<div class="row">
-			<div class="col-sm-4 col-12">
-				<a href="<?= base_url() ?>"><img src="<?=base_url()?>/dict_logo.png" alt="DICT Logo" style="max-width:55%;width: auto;height: auto;" /></a>
-			</div>
-
-			<div class="col-sm-8 col-12" style="text-align: right; font-size:calc(12px + 1.25vw);">
-				<span style="cursor:default">Leave Credit Computation</span></br>
-				<button ng-hide="<?=$this->ion_auth->logged_in()?1:0?>==false" href="#menu-toggle" class="btn btn-default" id="menu-toggle"><span class="fas fa-bars"></span></button>
-			</div>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+		<a class="navbar-brand" href="index.html">DICT Leave Credit Computation</a>
+		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarResponsive">
+			<ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
+				<li class="nav-item" data-toggle="tooltip" data-placement="right" title="Home">
+					<a class="nav-link" href="#">
+						<i class="fas fa-fw fa-home"></i>
+						<span class="nav-link-text">Home</span>
+					</a>
+				</li>
+				<li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
+					<a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents" data-parent="#exampleAccordion">
+						<i class="fas fa-fw fa-user-tie"></i>
+						<span class="nav-link-text">Employees</span>
+					</a>
+					<ul class="sidenav-second-level collapse" id="collapseComponents">
+						<li>
+							<a href="navbar.html">Navbar</a>
+						</li>
+						<li>
+							<a href="cards.html">Cards</a>
+						</li>
+					</ul>
+				</li>
+				<li class="nav-item" data-toggle="tooltip" data-placement="right" title="Example Pages">
+					<a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseExamplePages" data-parent="#exampleAccordion">
+						<i class="fas fa-fw fa-calendar-alt"></i>
+						<span class="nav-link-text">Example Pages</span>
+					</a>
+					<ul class="sidenav-second-level collapse" id="collapseExamplePages">
+						<li>
+							<a href="login.html">Login Page</a>
+						</li>
+						<li>
+							<a href="register.html">Registration Page</a>
+						</li>
+						<li>
+							<a href="forgot-password.html">Forgot Password Page</a>
+						</li>
+						<li>
+							<a href="blank.html">Blank Page</a>
+						</li>
+					</ul>
+				</li>
+				<li class="nav-item" data-toggle="tooltip" data-placement="right" title="Menu Levels">
+					<a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseMulti" data-parent="#exampleAccordion">
+						<i class="fa fa-fw fa-sitemap"></i>
+						<span class="nav-link-text">Menu Levels</span>
+					</a>
+					<ul class="sidenav-second-level collapse" id="collapseMulti">
+						<li>
+							<a href="#">Second Level Item</a>
+						</li>
+						<li>
+							<a href="#">Second Level Item</a>
+						</li>
+						<li>
+							<a href="#">Second Level Item</a>
+						</li>
+					</ul>
+				</li>
+				<li class="nav-item" data-toggle="tooltip" data-placement="right" title="Link">
+					<a class="nav-link" href="#">
+						<i class="fa fa-fw fa-link"></i>
+						<span class="nav-link-text">Link</span>
+					</a>
+				</li>
+			</ul>
+			<ul class="navbar-nav sidenav-toggler">
+				<li class="nav-item">
+					<a class="nav-link text-center" id="sidenavToggler">
+						<i class="fa fa-fw fa-angle-left"></i>
+					</a>
+				</li>
+			</ul>
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle mr-lg-2" id="messagesDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					<i class="fa fa-fw fa-envelope"></i>
+					<span class="d-lg-none">Messages
+						<span class="badge badge-pill badge-primary">12 New</span>
+					</span>
+					<span class="indicator text-primary d-none d-lg-block">
+						<i class="fa fa-fw fa-circle"></i>
+					</span>
+					</a>
+					<div class="dropdown-menu" aria-labelledby="messagesDropdown">
+						<h6 class="dropdown-header">New Messages:</h6>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="#">
+							<strong>David Miller</strong>
+							<span class="small float-right text-muted">11:21 AM</span>
+							<div class="dropdown-message small">Hey there! This new version of SB Admin is pretty awesome! These messages clip off when they reach the end of the box so they don't overflow over to the sides!</div>
+						</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="#">
+							<strong>Jane Smith</strong>
+							<span class="small float-right text-muted">11:21 AM</span>
+							<div class="dropdown-message small">I was wondering if you could meet for an appointment at 3:00 instead of 4:00. Thanks!</div>
+						</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="#">
+							<strong>John Doe</strong>
+							<span class="small float-right text-muted">11:21 AM</span>
+							<div class="dropdown-message small">I've sent the final files over to you for review. When you're able to sign off of them let me know and we can discuss distribution.</div>
+						</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item small" href="#">View all messages</a>
+					</div>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle mr-lg-2" id="alertsDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<i class="fa fa-fw fa-bell"></i>
+						<span class="d-lg-none">Alerts
+							<span class="badge badge-pill badge-warning">6 New</span>
+						</span>
+						<span class="indicator text-warning d-none d-lg-block">
+							<i class="fa fa-fw fa-circle"></i>
+						</span>
+					</a>
+					<div class="dropdown-menu" aria-labelledby="alertsDropdown">
+						<h6 class="dropdown-header">New Alerts:</h6>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="#">
+							<span class="text-success">
+								<strong>
+								<i class="fa fa-long-arrow-up fa-fw"></i>Status Update</strong>
+							</span>
+							<span class="small float-right text-muted">11:21 AM</span>
+							<div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
+						</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="#">
+							<span class="text-danger">
+								<strong>
+								<i class="fa fa-long-arrow-down fa-fw"></i>Status Update</strong>
+							</span>
+							<span class="small float-right text-muted">11:21 AM</span>
+							<div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
+						</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="#">
+							<span class="text-success">
+								<strong>
+								<i class="fa fa-long-arrow-up fa-fw"></i>Status Update</strong>
+							</span>
+							<span class="small float-right text-muted">11:21 AM</span>
+							<div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
+						</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item small" href="#">View all alerts</a>
+					</div>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" data-toggle="modal" data-target="#exampleModal">
+					<i class="fa fa-fw fa-sign-out"></i>Logout</a>
+				</li>
+			</ul>
 		</div>
-	</header>
-	<!-- /#header -->
-	<div id="container">
+	</nav>
