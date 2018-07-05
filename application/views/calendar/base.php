832022988936a34bf96d7094002ad1c8e26e3228
TjDMC <tjustin.asdf@outlook.com>
2018-07-05 12:02:04 +0800

    calendar

--- application/views/calendar/base.php
+++ application/views/calendar/base.php
@@ -11,19 +11,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     color:lightgray;
 }
 
+.calendar .date-cells:hover{
+    background-color:lightgray;
+    cursor:pointer;
+}
+
 </style>
 
-<div class="card" ng-controller="calendar_display" ng-init="init()">
+<div class="card" ng-controller="calendar_display" ng-init="init(null)">
     <div class="card-header">
         <h2>Calendar</h2>
     </div>
     <div class="card-body">
-        <div class="table-responsive mx-auto" style="max-width:1200px">
-            <div class="row mb-3 justify-content-between container">
-                <h3 class="col-5 text-left">{{formatCurrentDate()}}</h3>
-                <div class="row col-2 btn-group justify-content-end">
-                    <button ng-click="addMonth(-1)" class="col-1 btn btn-outline-dark my-auto" style="max-width:50px;max-height:50px"><i class="fas fa-angle-left fa-2x"></i></button>
-                    <button ng-click="addMonth(1)" class="col-1 btn btn-outline-dark my-auto" style="max-width:50px;max-height:50px"><i class="fas fa-angle-right fa-2x"></i></button>
+        <div class="table-responsive container mx-auto" style="max-width:1200px">
+            <div class="row mb-3 justify-content-between">
+                <h3 class="col-5 text-left my-auto">{{formatCurrentDate()}}</h3>
+                <div class="col my-auto btn-group">
+                    <div class="btn-group">
+                        <button class="btn btn-outline-secondary" type="button" ng-click="setCurrentDate(moment())">Today</button>
+                        <a  class="dropdown-toggle btn btn-outline-secondary" id="navigateDate" role="button" data-toggle="dropdown" data-target="#" href="#">Navigate</a>
+                        <ul class="dropdown-menu" role="menu">
+                            <datetimepicker data-ng-model="navigateDate" data-on-set-time="setCurrentDate(newDate)" data-datetimepicker-config="{ dropdownSelector: '#navigateDate',minView:'month',startView:'month' }"/>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-2 btn-group justify-content-end">
+                    <button ng-click="addMonth(-1)" class="btn btn-outline-dark my-auto" style="max-width:50px;max-height:50px"><i class="fas fa-angle-left fa-2x"></i></button>
+                    <button ng-click="addMonth(1)" class="btn btn-outline-dark my-auto" style="max-width:50px;max-height:50px"><i class="fas fa-angle-right fa-2x"></i></button>
                 </div>
             </div>
             <table class="table table-bordered calendar">
@@ -37,11 +51,52 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     <th>Saturday</th>
                 </tr>
                 <tr ng-repeat="row in calendar">
-                    <td ng-repeat="date in row" style="height:110px;width:14%">
+                    <td class="date-cells" ng-repeat="date in row" style="height:110px;width:14%" ng-click="showModal(date)">
                         <span ng-class="{'extra-days':date.month()!==currentDate.month()}">{{date.date()}}</span>
+                        <div>
+                            <p class="font-italic" ng-repeat="event in date.events">{{event}}.</p>
+                        </div>
                     </td>
                 </tr>
             </table>
+        </div>
+    </div>
+
+    <div class="modal fade" id="eventModal" tabindex="-1" role="dialog" aria-labelledby="eventLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="eventLabel">Add/Edit Event</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="form-group">
+                            <p>Date: <span class="font-weight-bold">{{selectedDate.format('MMMM DD, YYYY')}}</span></p>
+                        </div>
+                        <div class="form-group">
+                            <p>Events:</p>
+                            <div class="form-group" ng-repeat="event in selectedDate.events track by $index">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">#{{$index+1}}</span>
+                                    </div>
+                                    <input type="text" class="form-control" ng-model="selectedDate.events[$index]"></input>
+                                    <div class="input-group-append">
+                                        <button class="btn btn-outline-dark" ng-click="addOrDeleteEvent(selectedDate.events,$index)"><i class="fas fa-times"></i></button>
+                                    </div>
+                                </div>
+                            </div>
+                            <button class="btn btn-info" type="button" ng-click="addOrDeleteEvent(selectedDate.events)">Add Event</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- js/app_calendar.js
+++ js/app_calendar.js
@@ -1,11 +1,39 @@
 app.controller('calendar_display',function($scope,$rootScope,$window){
 
     $scope.currentDate = '';
+    /*Structure of events:
+        events:[
+            {
+                date:date1,
+                events:[event1,event2,...]
+            },
+            {
+                date:date2,
+                events:[event1,event2,...]
+            }...
+        ]
+        Date is formatted as: yyyy-mm-dd when coming into and out of angular
+    */
+    $scope.events = [
+        {
+            date:'2018-01-01',
+            events:['New Year']
+        },
+        {
+            date:'2018-06-18',
+            events:['Intern boiis came to town','Oh yeah baby']
+        }
+    ];
     $scope.calendar;
-    $scope.init = function(){
+    $scope.selectedDate ={};
+
+    $scope.init = function(events){
         $scope.currentDate = moment();
         $scope.calendar = $scope.getCalendar();
+        //$scope.events = events;
     }
+
+    $scope.moment = moment;
 
     $scope.formatCurrentDate = function(){
         return $scope.currentDate.format('MMMM YYYY');
@@ -14,6 +42,43 @@ app.controller('calendar_display',function($scope,$rootScope,$window){
     $scope.addMonth = function(x){
         $scope.currentDate.add(x,'month');
         $scope.calendar = $scope.getCalendar($scope.currentDate);
+    }
+
+    $scope.setCurrentDate = function(date){
+        $scope.currentDate = moment(date);
+        $scope.calendar = $scope.getCalendar(date);
+    }
+
+    $scope.showModal = function(dateEvent){ //dateEvent are moment objects that have their respective events associated to them
+        $scope.selectedDate = dateEvent;
+        if(!$scope.selectedDate.events){
+            $scope.selectedDate.events = [];
+        }
+        angular.element('#eventModal').modal('show');
+    }
+
+    $scope.addOrDeleteEvent = function(events,index=-1){
+
+        for(var i = 0 ; i<events.length ; i++){
+            for(var j = 0 ; j<events.length ; j++){
+                if(i!==j&&events[i]===events[j]){
+                    $rootScope.showCustomModal('Warning','Please make sure there are no duplicate events',function(){angular.element('#customModal').modal('hide');},function(){},'Ok');
+                    return;
+                }
+            }
+        }
+
+        //delete if index is not -1
+        console.log(events);
+        if(index>-1){
+            events.splice(index,1);
+        }else{
+            if(events.indexOf('')>-1){
+                $rootScope.showCustomModal('Warning','Please fill-in the events first before adding more.',function(){angular.element('#customModal').modal('hide');},function(){},'Ok');
+                return;
+            }
+            events.push('');
+        }
     }
 
     $scope.getCalendar = function(date = null){
@@ -56,13 +121,17 @@ app.controller('calendar_display',function($scope,$rootScope,$window){
             row = [];
         }
 
-        /*for(var i = 0 ; i<calendar.length ; i++){
-            var string = '';
-            for(var j = 0 ; j<calendar[i].length ; j++){
-                string+='  '+calendar[i][j].date()<10 ? '  '+calendar[i][j].date():' '+calendar[i][j].date();
+        //Associate events to dates
+        for(var i = 0 ; i<calendar.length ; i++){
+            for(var j = 0 ; j < calendar[i].length ; j++){
+                for(var k = 0 ; k<$scope.events.length ; k++){
+                    if(calendar[i][j].isSame(moment($scope.events[k].date))){
+                        calendar[i][j].events = $scope.events[k].events;
+                    }
+                }
             }
-            console.log(string);
-        }*/
+        }
+        console.log(calendar);
         return calendar;
     }
 });
