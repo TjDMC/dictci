98095bb85eb6f0c24fd1c6a4b1a66473a3f1502e
ahotsuki <steinbergcurse@gmail.com>
2018-07-10 15:47:39 +0800

    asd

--- application/views/employee/form.php
+++ application/views/employee/form.php
@@ -3,18 +3,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <div ng-controller="employee_display" ng-init='init(<?=$employee ?>,<?=$leaves?>)'>
 	<div style="text-align:center">
-		<p>RECORD OF LEAVES OF ABSENCE</p>
-		<p>(UNDER R.A. 2266)</p>
+		<p style="font-size:25px"><b>RECORD OF LEAVES OF ABSENCE</b></p>
+		<p><b>(UNDER R.A. 2266)</b></p>
 	</div>
 	<div style="background-color:#000000">
 		<div style="width:auto;float:left">
-			<p>NAME:&nbsp;&nbsp;<u>{{employee.last_name}}, {{employee.first_name}} {{employee.middle_name}}</u><p>
+			<p style="font-size:18px"><b>NAME:</b>&nbsp;&nbsp;<u>&nbsp;&nbsp;{{employee.last_name}}, {{employee.first_name}} {{employee.middle_name}}&nbsp;&nbsp;</u><p>
 		</div>
 		<div style="width:auto;float:right">
-			<p>Date Entered Service:&nbsp;&nbsp;<u>{{employee.first_day}}</u></p>
+			<p style="font-size:18px"><b>Date Entered Service:</b>&nbsp;&nbsp;<u>&nbsp;&nbsp;{{employee.first_day}}&nbsp;&nbsp;</u></p>
 		</div>
 	</div>
-	<div style="clear:center">
 	<table class="table table-bordered" style="text-align:center;width:100%;table-layout:abs">
 		<col width="50px">
 		<col width="50px">
@@ -47,12 +46,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<th style="vertical-align:middle">SICK</th>
 			<th style="vertical-align:middle">VAC</th>
 			<th style="vertical-align:middle">SICK</th>
-			<th style="vertical-align:middle"><font size="1">HOUR</font></th>
-			<th style="vertical-align:middle"><font size="1">MIN</font></th>
-			<th style="vertical-align:middle"><font size="1">TOTAL</font></th>
-			<th style="vertical-align:middle"><font size="1">HOUR</font></th>
-			<th style="vertical-align:middle"><font size="1">MIN</font></th>
-			<th style="vertical-align:middle"><font size="1">TOTAL</font></th>
+			<th style="vertical-align:middle"><font size="2">HOUR</font></th>
+			<th style="vertical-align:middle"><font size="2">MIN</font></th>
+			<th style="vertical-align:middle"><font size="2">TOTAL</font></th>
+			<th style="vertical-align:middle"><font size="2">HOUR</font></th>
+			<th style="vertical-align:middle"><font size="2">MIN</font></th>
+			<th style="vertical-align:middle"><font size="2">TOTAL</font></th>
 			<th style="vertical-align:middle">VAC</th>
 			<th style="vertical-align:middle">SICK</th>
 		</tr>
@@ -73,5 +72,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<td style="vertical-align:middle"><font size="2">value{{i}}</font></td>
 		</tr>
 	</table>
-	</div>
 </div>
