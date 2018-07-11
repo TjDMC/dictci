07b736306860284e3365ef87aaa2a16b49fa5365
ahotsuki <steinbergcurse@gmail.com>
2018-07-11 12:37:16 +0800

    emp

--- application/views/employee/form.php
+++ application/views/employee/form.php
@@ -14,66 +14,68 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<p style="font-size:18px"><b>Date Entered Service:</b>&nbsp;&nbsp;<span>&nbsp;&nbsp;{{employee.first_day}}&nbsp;&nbsp;</span></p>
 		</div>
 	</div>
-	<table class="table table-bordered table-sm" style="text-align:center;width:100%">
-	<colgroup>
-		<col width="7%">
-		<col width="7%">
-		<col width="17%">
-		<col width="7%">
-		<col width="7%">
-		<col width="4%">
-		<col width="4%">
-		<col width="4%">
-		<col width="4%">
-		<col width="4%">
-		<col width="4%">
-		<col width="7%">
-		<col width="7%">
-		<col width="17%">
-	</colgroup>
-		<tr>
-			<th colspan="2" rowspan="2" style="vertical-align:middle">Leave Earned</th>
-			<th rowspan="3" style="vertical-align:middle">WHEN TAKEN</th>
-			<th colspan="2" rowspan="2" style="vertical-align:middle">LEAVES TAKEN</th>
-			<th colspan="3" style="vertical-align:middle">UNDERTIME</th>
-			<th rowspan="2" colspan="3" style="vertical-align:middle">W/O PAY</th>
-			<th colspan="2" rowspan="2" style="vertical-align:middle">BALANCE</th>
-			<th rowspan="3" style="vertical-align:middle">REMARK</th>
-		</tr>
-		<tr>
-			<th colspan="3">VAC</th>
-		</tr>
-		<tr>
-			<th style="vertical-align:middle">VAC</th>
-			<th style="vertical-align:middle">SICK</th>
-			<th style="vertical-align:middle">VAC</th>
-			<th style="vertical-align:middle">SICK</th>
-			<th style="vertical-align:middle"><font size="2">HOUR</font></th>
-			<th style="vertical-align:middle"><font size="2">MIN</font></th>
-			<th style="vertical-align:middle"><font size="2">TOTAL</font></th>
-			<th style="vertical-align:middle"><font size="2">HOUR</font></th>
-			<th style="vertical-align:middle"><font size="2">MIN</font></th>
-			<th style="vertical-align:middle"><font size="2">TOTAL</font></th>
-			<th style="vertical-align:middle">VAC</th>
-			<th style="vertical-align:middle">SICK</th>
-		</tr>
-		<tbody ng-repeat="leave in leaves">
-		<tr ng-repeat="range in leave.date_ranges">
-			<td style="vertical-align:middle"><font size="2"></font></td>
-			<td style="vertical-align:middle"><font size="2"></font></td>
-			<td style="vertical-align:middle"><font size="2">{{range.start_date}}-{{range.end_date}}</font></td>
-			<td style="vertical-align:middle"><font size="2"></font></td>
-			<td style="vertical-align:middle"><font size="2"></font></td>
-			<td style="vertical-align:middle"><font size="2">{{range.hours}}</font></td>
-			<td style="vertical-align:middle"><font size="2">{{range.minutes}}</font></td>
-			<td style="vertical-align:middle"><font size="2">{{Math.round((range.hours*60 + range.minutes)/60)}}</font></td>
-			<td style="vertical-align:middle"><font size="2"></font></td>
-			<td style="vertical-align:middle"><font size="2"></font></td>
-			<td style="vertical-align:middle"><font size="2"></font></td>
-			<td style="vertical-align:middle"><font size="2"></font></td>
-			<td style="vertical-align:middle"><font size="2"></font></td>
-			<td style="vertical-align:middle"><font size="2">{{leave.info.remarks}}</font></td>
-		</tr>
-		</tbody>
-	</table>
+	<div class="table-responsive">
+		<table class="table table-bordered table-sm" style="text-align:center;width:100%">
+		<colgroup>
+			<col width="7%">
+			<col width="7%">
+			<col width="17%">
+			<col width="7%">
+			<col width="7%">
+			<col width="4%">
+			<col width="4%">
+			<col width="4%">
+			<col width="4%">
+			<col width="4%">
+			<col width="4%">
+			<col width="7%">
+			<col width="7%">
+			<col width="17%">
+		</colgroup>
+			<tr>
+				<th colspan="2" rowspan="2" style="vertical-align:middle">Leave Earned</th>
+				<th rowspan="3" style="vertical-align:middle">WHEN TAKEN</th>
+				<th colspan="2" rowspan="2" style="vertical-align:middle">LEAVES TAKEN</th>
+				<th colspan="3" style="vertical-align:middle">UNDERTIME</th>
+				<th rowspan="2" colspan="3" style="vertical-align:middle">W/O PAY</th>
+				<th colspan="2" rowspan="2" style="vertical-align:middle">BALANCE</th>
+				<th rowspan="3" style="vertical-align:middle">REMARK</th>
+			</tr>
+			<tr>
+				<th colspan="3">VAC</th>
+			</tr>
+			<tr>
+				<th style="vertical-align:middle">VAC</th>
+				<th style="vertical-align:middle">SICK</th>
+				<th style="vertical-align:middle">VAC</th>
+				<th style="vertical-align:middle">SICK</th>
+				<th style="vertical-align:middle"><font size="2">HOUR</font></th>
+				<th style="vertical-align:middle"><font size="2">MIN</font></th>
+				<th style="vertical-align:middle"><font size="2">TOTAL</font></th>
+				<th style="vertical-align:middle"><font size="2">HOUR</font></th>
+				<th style="vertical-align:middle"><font size="2">MIN</font></th>
+				<th style="vertical-align:middle"><font size="2">TOTAL</font></th>
+				<th style="vertical-align:middle">VAC</th>
+				<th style="vertical-align:middle">SICK</th>
+			</tr>
+			<tbody ng-repeat="leave in leaves">
+				<tr ng-repeat="range in leave.date_ranges">
+					<td style="vertical-align:middle"><font size="2"></font></td>
+					<td style="vertical-align:middle"><font size="2"></font></td>
+					<td style="vertical-align:middle"><font size="2">{{range.start_date}}-{{range.end_date}}</font></td>
+					<td style="vertical-align:middle"><font size="2">{{leave.info.type=='Vacation' ? getDeductedCredits(leave.info.type,range) : ''}}</font></td>
+					<td style="vertical-align:middle"><font size="2">{{leave.info.type=='Sick' ? getDeductedCredits(leave.info.type,range) : ''}}</font></td>
+					<td style="vertical-align:middle"><font size="2">{{range.hours}}</font></td>
+					<td style="vertical-align:middle"><font size="2">{{range.minutes}}</font></td>
+					<td style="vertical-align:middle"><font size="2">{{((range.hours*60 + range.minutes)/60) | number:2}}</font></td>
+					<td style="vertical-align:middle"><font size="2"></font></td>
+					<td style="vertical-align:middle"><font size="2"></font></td>
+					<td style="vertical-align:middle"><font size="2"></font></td>
+					<td style="vertical-align:middle"><font size="2"></font></td>
+					<td style="vertical-align:middle"><font size="2"></font></td>
+					<td style="vertical-align:middle"><font size="2">{{leave.info.remarks}}</font></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </div>
