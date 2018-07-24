672c67bce1d37636c8112dd5cc92f8b76146d6c0
TjDMC <tjustin.asdf@outlook.com>
2018-07-24 14:59:49 +0800

    navigation layouting

--- application/views/employee/navigation.php
+++ application/views/employee/navigation.php
@@ -31,7 +31,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     				<input type="text" class="form-control" ng-model="searchTerm">
     			</div>
     			<div class="col row align-items-center justify-content-center container">
-                    <span class="col text-right" style="min-width:220px">Showing {{(page-1)*limit+1}} to {{(page-1)*limit+getDisplayNumber()}} of {{filteredEmployees.length}} results</span>
+                    <span class="col text-lg-right text-sm-center" style="min-width:219px">Showing {{(page-1)*limit+1}} to {{(page-1)*limit+getDisplayNumber()}} of {{filteredEmployees.length}} results</span>
 
                     <span class="col justify-content-center text-center" style="min-width:205px">
                         <span class="nav-button" ng-class="{disabled:(page<=1)}" ng-click="page=1"><i class="fas fa-angle-double-left fa-fw"></i></span>
@@ -44,7 +44,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         <span class="nav-button" ng-class="{disabled:(page>=getMaxPage())}" ng-click="page=getMaxPage()"><i class="fas fa-angle-double-right fa-fw"></i></span>
                     </span>
 
-                    <span class="col align-items-center text-left" style="min-width:232px">
+                    <span class="col align-items-center text-xl-right text-sm-center" style="min-width:232px">
                         <span class="mr-1">Results Per Page:</span>
                         <select class="custom-select" ng-model="limit" style="max-width:75px;min-width:75px">
                             <option ng-value="5">5</option>
